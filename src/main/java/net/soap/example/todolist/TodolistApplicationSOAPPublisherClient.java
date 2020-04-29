a5ce4755079e22dec2f19ea04a3b77d394ded35e
Grigoris Mavridakis <11526485+gmavridakis@users.noreply.github.com>
2020-04-29 18:08:13 +0300

    fix spaces

--- src/main/java/net/soap/example/todolist/TodolistApplicationSOAPPublisherClient.java
+++ src/main/java/net/soap/example/todolist/TodolistApplicationSOAPPublisherClient.java
@@ -30,16 +30,17 @@ public class TodolistApplicationSOAPPublisherClient {
 			TaskService service = svc.getPort(TaskService.class);
 	        
 			System.out.println("Add Task Status (t1) 	: " +service.addTask(t1));
-	        System.out.println("Add Task Status (t2) 	: " +service.addTask(t2));
-	        //get a task
-	        System.out.println("Get a Task (t1) 	: " +service.getTask(1));
-	        //get all tasks
-	        System.out.println("Show all tasks 		: " +Arrays.asList(service.getAllTasks()));
-	        //delete task
-	        System.out.println("Delete Task Status (t2) : "+service.deleteTask(2));
-	        //get all tasks
-	        System.out.println("Show all tasks 		: " +Arrays.asList(service.getAllTasks()));			
-	        System.out.println("***************");
+			System.out.println("Add Task Status (t2) 	: " +service.addTask(t2));
+			//get a task
+			System.out.println("Get a Task (t1) 	: " +service.getTask(1));
+			//get all tasks
+			System.out.println("Show all tasks 		: " +Arrays.asList(service.getAllTasks()));
+			//delete task
+			System.out.println("Delete Task Status (t2) : "+service.deleteTask(2));
+			//get all tasks
+			System.out.println("Show all tasks 		: " +Arrays.asList(service.getAllTasks()));			
+			System.out.println("***************");
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
